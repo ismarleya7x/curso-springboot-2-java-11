0b868019147ce142f922e8878d0acef3efab11e1
IsmarleyA7X <ismarley_2005@msn.com>
2020-09-22 20:08:01 -0300

    Order, Instant, ISO 8601 part 2

---
+++ src/main/java/com/ismarleycarvalho/course/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.ismarleycarvalho.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ismarleycarvalho.course.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
