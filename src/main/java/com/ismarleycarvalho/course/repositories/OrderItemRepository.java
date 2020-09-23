de5bea9f6ed9028adc22ec98bd0af3be1d2928b3
IsmarleyA7X <ismarley_2005@msn.com>
2020-09-23 18:18:11 -0300

    OrderItem, manyTomany association with extra attrs PART2

---
+++ src/main/java/com/ismarleycarvalho/course/repositories/OrderItemRepository.java
@@ -0,0 +1,9 @@
+package com.ismarleycarvalho.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ismarleycarvalho.course.entities.OrderItem;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+
+}
